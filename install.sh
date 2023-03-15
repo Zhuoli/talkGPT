de25e4eeddd558b238bec581f25a4f0602a0d2a8
zhuoli liang <zhuoliseattle@gmail.com>
2023-03-14 21:46:15 -0700

    update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
-brew install portaudio
-brew install flac
-brew install ffmpeg
+#!/bin/bash
+set -e
+#brew install portaudio
+#brew install flac
+#brew install ffmpeg
+sudo apt-get install python3-all-dev
+sudo apt-get install portaudio19-dev
+sudo apt-get install python3-pyaudio
 
 pip install openai
+pip install soundfile
 pip install pyaudio
 pip install -U openai-whisper
 pip install SpeechRecognition
