556a237c693e93a2ee2c3f9a21dde594b1ea9657
Zhuoli Liang <zhuoliseattle@gmail.com>
2023-03-15 22:31:41 -0700

    resolve conflicts

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-<<<<<<< HEAD
+
 #brew install portaudio
 #brew install flac
 #brew install ffmpeg
@@ -10,14 +10,11 @@ choco install ffmpeg # https://github.com/openai/whisper
 sudo apt-get install python3-all-dev
 sudo apt-get install portaudio19-dev
 sudo apt-get install python3-pyaudio
-=======
+
 brew install portaudio
 brew install flac
 brew install ffmpeg
-#sudo apt-get install python3-all-dev
-#sudo apt-get install portaudio19-dev
-#sudo apt-get install python3-pyaudio
->>>>>>> 5e721d82cc6d8e8295f714cd037fe51558338ae3
+
 
 pip install openai
 pip install soundfile
